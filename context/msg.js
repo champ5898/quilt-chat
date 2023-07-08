cb4eca5dd44add98db847cbf6e6d4b33dc6562f5
champ5898 <daschandan83333@gmail.com>
2023-07-08 11:11:26 +0530

    feat: msg

--- context/msg.js
+++ context/msg.js
@@ -5,13 +5,6 @@ import React, { useState } from "react";
 
 import { GET_P2P_MESSAGEFEED, SEND_P2P_MESSAGE } from "@/graphql/queries";
 
-const [sendP2pMessage] = useMutation(SEND_P2P_MESSAGE, {
-  onCompleted: (data) => {},
-  onError: (error) => {
-    console.error(error);
-  },
-});
-
 const data = {
   receiverAddress: "0x77b72a66C24c24523dE4ce41e871d57550b64b51",
   messageType: "message",
@@ -19,11 +12,16 @@ const data = {
   sentAt: "2023-04-15T11:08:46.098+00:00",
 };
 
-sendP2pMessage({
-  variables: { data: data },
-});
-
 export const msg = () => {
+  const [sendP2pMessage] = useMutation(SEND_P2P_MESSAGE, {
+    onCompleted: (data) => {},
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+  sendP2pMessage({
+    variables: { data: data },
+  });
   const [address, setaddress] = useState(
     "tz1Sw1gGA6bi2drimg1yUYF6ZcTsmaCyNnuw"
   );
